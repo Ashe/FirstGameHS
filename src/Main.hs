4c79579e203f3a0c95b10c60be15b89718f48db7
Ashley Smith <ashley@aas.sh>
2018-03-04 02:38:13 +0000

    Started working on 'GameStates'

--- src/Main.hs
+++ src/Main.hs
@@ -19,21 +19,30 @@ screenWidth, screenHeight :: CInt
 frameLimit :: Int
 frameLimit = 60
 
+data GameState
+  = State {
+      entities :: Guy
+    , uselessIntForCompilerWarning :: Int
+  }
+
 -- This is our game world. It only consists of one lonely guy
 -- who has a position and a velocity
-data World
+data Guy
     = Guy
     { position :: Point V2 CDouble
     , velocity :: V2 CDouble
     } deriving (Show, Eq)
 
--- Our initial world starts out with the guy roughly in the middle
-initialGuy :: World
+-- Our initial guy starts out with him roughly in the middle
+initialGuy :: Guy
 initialGuy =
     Guy
     { position = P $ V2 (fromIntegral screenWidth / 2) (fromIntegral $ screenHeight - 100)
     , velocity = V2 0 0
     }
+
+initialState :: GameState
+initialState = State initialGuy 0
 
 jumpVelocity :: V2 CDouble
 jumpVelocity = V2 0 (-2)
@@ -50,34 +59,34 @@ pattern KeyPressed a <- (SDL.KeyboardEvent (SDL.KeyboardEventData _ SDL.Pressed 
 pattern KeyReleased a <- (SDL.KeyboardEvent (SDL.KeyboardEventData _ SDL.Released _ (SDL.Keysym _ a _)))
 
 
--- This processed input and modifies velocities of things in our world accordingly
+-- @TODO: This processed input and modifies velocities of things in our world accordingly
 -- and then returns the new world
-processInput :: World -> SDL.EventPayload -> World
-processInput world@(Guy _ curVel) (KeyPressed SDL.KeycodeUp) =
-    world { velocity = curVel * V2 1 0 + jumpVelocity}
-processInput world@(Guy _ curVel) (KeyPressed SDL.KeycodeLeft) =
-    world { velocity = negate walkingSpeed + curVel  }
-processInput world@(Guy _ curVel) (KeyPressed SDL.KeycodeRight) =
-    world { velocity = walkingSpeed + curVel  }
+-- processInput :: World -> SDL.EventPayload -> World
+-- processInput world@(Guy _ curVel) (KeyPressed SDL.KeycodeUp) =
+--     world { velocity = curVel * V2 1 0 + jumpVelocity}
+-- processInput world@(Guy _ curVel) (KeyPressed SDL.KeycodeLeft) =
+--     world { velocity = negate walkingSpeed + curVel  }
+-- processInput world@(Guy _ curVel) (KeyPressed SDL.KeycodeRight) =
+--     world { velocity = walkingSpeed + curVel  }
+--
+-- processInput world@(Guy _ curVel) (KeyReleased SDL.KeycodeUp) =
+--     world { velocity = curVel - jumpVelocity }
+-- processInput world@(Guy _ curVel) (KeyReleased SDL.KeycodeLeft) =
+--     world { velocity = curVel - negate walkingSpeed  }
+-- processInput world@(Guy _ curVel) (KeyReleased SDL.KeycodeRight) =
+--     world { velocity = curVel - walkingSpeed  }
+-- processInput w _ = w
 
-processInput world@(Guy _ curVel) (KeyReleased SDL.KeycodeUp) =
-    world { velocity = curVel - jumpVelocity }
-processInput world@(Guy _ curVel) (KeyReleased SDL.KeycodeLeft) =
-    world { velocity = curVel - negate walkingSpeed  }
-processInput world@(Guy _ curVel) (KeyReleased SDL.KeycodeRight) =
-    world { velocity = curVel - walkingSpeed  }
-processInput w _ = w
 
-
--- This function takes cares of applying things like our entities' velocities
+-- @TODO: This function takes cares of applying things like our entities' velocities
 -- to their positions, as well as
-updateWorld :: CDouble -> World -> World
-updateWorld delta (Guy (P pos) vel) =
-    let (V2 newPosX newPosY) = pos + (gravity + vel) * V2 delta delta
-        -- Ensure that we stay within bounds
-        fixedX = max 0 $ min newPosX (fromIntegral screenWidth - 50)
-        fixedY = max 0 $ min (fromIntegral screenHeight - 100) newPosY
-    in Guy (P $ V2 fixedX fixedY) vel
+-- updateWorld :: CDouble -> World -> World
+-- updateWorld delta (Guy (P pos) vel) =
+--     let (V2 newPosX newPosY) = pos + (gravity + vel) * V2 delta delta
+--         -- Ensure that we stay within bounds
+--         fixedX = max 0 $ min newPosX (fromIntegral screenWidth - 50)
+--         fixedY = max 0 $ min (fromIntegral screenHeight - 100) newPosY
+--     in Guy (P $ V2 fixedX fixedY) vel
 
 
 main :: IO ()
@@ -111,7 +120,7 @@ main = do
   SDL.freeSurface xOutSurface
 
 
-  let loop last world = do
+  let loop last state = do
         events <- SDL.pollEvents
 
         -- Need to calculate the time delta
@@ -122,9 +131,10 @@ main = do
             payloads = map SDL.eventPayload events
             quit = SDL.QuitEvent `elem` payloads
 
-        -- Update functions
-        let worldAfterInput = foldl' processInput world payloads
-            newWorld        = updateWorld delta worldAfterInput
+        -- @TODO: Update functions
+        --let worldAfterInput = foldl' processInput state payloads
+        --    newWorld        = updateWorld delta worldAfterInput
+        let newState = state
 
         SDL.clear renderer
 
@@ -133,17 +143,17 @@ main = do
         -- Draw our world(guy) as a white rectangle
         let drawColor = SDL.rendererDrawColor renderer
         drawColor $= V4 255 255 255 0
-        SDL.fillRect renderer . Just $ SDL.Rectangle (truncate <$> position newWorld) (V2 50 100)
+        SDL.fillRect renderer . Just $ SDL.Rectangle (truncate <$> position (entities newState)) (V2 50 100)
 
         -- My attempt at an FPS limit. I don't write games so it is possible this is incorrect
         let frameDelay = 1000 / fromIntegral frameLimit
         when (delta < frameDelay) $ SDL.delay (truncate $ frameDelay - delta)
 
         SDL.present renderer
-        unless quit $ loop now newWorld
+        unless quit $ loop now newState
 
   now <- SDL.getPerformanceCounter
-  loop now initialGuy
+  loop now initialState
 
   SDL.destroyWindow window
   SDL.quit
